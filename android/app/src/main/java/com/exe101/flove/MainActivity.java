6919fff5f51aa31947ce9d72951979ce5fc7b419
Binh Thanh <thanh14022005@gmail.com>
2026-01-13 15:56:28 +0700

    feat(mobile): add Capacitor Android project

---
+++ android/app/src/main/java/com/exe101/flove/MainActivity.java
@@ -0,0 +1,5 @@
+package com.exe101.flove;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.exe101.flove',
+  appName: 'F-Love',
+  webDir: 'dist'
+};
+
+export default config;
